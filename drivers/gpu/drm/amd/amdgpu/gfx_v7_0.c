941c92160f3ddf4ea3c5a424f8ae67fb8d6911f8
Niels Westphal (crashniels) <nielswest@live.de>
2025-07-03 18:26:29 +0200

    Follow Liverpool raster format

--- drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
@@ -2226,8 +2226,12 @@ gfx_v7_0_raster_config(struct amdgpu_device *adev, u32 *rconf, u32 *rconf1)
 		*rconf1 |= 0x0;
 		break;
 	case CHIP_GLADIUS:
-		*rconf |= 0x2a00161a;
-		*rconf1 |= 0x0000002e;
+		*rconf |= RB_MAP_PKR0(2) | RB_MAP_PKR1(2) |
+			  RB_XSEL2(1) | PKR_MAP(2) | PKR_XSEL(1) |
+			  PKR_YSEL(1) | SE_MAP(2) | SE_XSEL(2) |
+			  SE_YSEL(2);
+		*rconf1 |= SE_PAIR_MAP(2) | SE_PAIR_XSEL(3) |
+			   SE_PAIR_YSEL(2);
 		break;
 	default:
 		DRM_ERROR("unknown asic: 0x%x\n", adev->asic_type);
