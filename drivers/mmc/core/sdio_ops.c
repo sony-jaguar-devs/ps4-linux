7257ec39549948cd66c6534188d19daaeb76fca8
feeRnt <81442162+feeRnt@users.noreply.github.com>
2025-07-24 14:05:29 +0000

    3 retries on each MMC CMD52 or 53. Possibly needed for Marvell cards according to patch.

--- drivers/mmc/core/sdio_ops.c
+++ drivers/mmc/core/sdio_ops.c
@@ -80,7 +80,11 @@ static int mmc_io_rw_direct_host(struct mmc_host *host, int write, unsigned fn,
 	cmd.arg |= in;
 	cmd.flags = MMC_RSP_SPI_R5 | MMC_RSP_R5 | MMC_CMD_AC;
 
-	err = mmc_wait_for_cmd(host, &cmd, 0);
+	/* err = mmc_wait_for_cmd(host, &cmd, 0);
+	 * https://fa.linux.kernel.narkive.com/t6hn0noQ/patch-mmc-sdio-retry-cmd52-53-when-error-happens
+	 * Use 3 retries instead of 0 retries:
+	 */
+	err = mmc_wait_for_cmd(host, &cmd, MMC_CMD_RETRIES);
 	if (err)
 		return err;
 
